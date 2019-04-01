445e0f6c5ec881692d6123e8e8c2f29e29606ab6
Ian Alexander <ian.andrew.alexander@gmail.com>
2019-04-01 07:55:35 +0100

    Adds example

---
+++ app/src/main/java/com/example/androidkeyboardstatechecker/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.androidkeyboardstatechecker
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        KeyboardEventListener(this) {
+            Log.v("Keyboard checker", "Keyboard is open = $it")
+        }
+    }
+}
